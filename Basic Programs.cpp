439e81b366a0c497b8df803c0dc6fc5d55df2b8c
Tanushwi <156989068+Tanushwi@users.noreply.github.com>
2024-02-01 11:16:16 +0530

    Add files via upload

---
+++ Basic Programs.cpp
@@ -0,0 +1,95 @@
+//Q1. Sum of two numbers.
+//#include<stdio.h>
+//int main()
+//{
+//	int a,b,c;
+//	printf("\nEnter two numbers:");
+//	scanf("%d %d",&a,&b);
+//	c=a+b;
+//	printf("\nThe sum is:%d",c);
+//	return 0;
+//}
+
+//Q2. Area of rectangle.
+//#include<stdio.h>
+//int main()
+//{
+//	int l,b,area;
+//	printf("\nEnter length and breadth:");
+//	scanf("%d %d",&l,&b);
+//	area=l*b;
+//	printf("\nThe area of rectangle is:%d",area);
+//	return 0;
+//}
+
+//Q3. Perimeter of rectangle.
+//#include<stdio.h>
+//int main()
+//{
+//	int l,b,per;
+//	printf("\nEnter length and breadth:");
+//	scanf("%d %d",&l,&b);
+//	per=2*(l+b);
+//	printf("\nThe perimeter of rectangle is: %d",per);
+//	return 0;
+//}
+
+//Q4. Area of circle.
+//#include<stdio.h>
+//#define pie 3.14
+//int main()
+//{
+//	int r;
+//	float area;
+//	printf("\nEnter radius:");
+//	scanf("%d",&r);
+//	area=pie*r*r;
+//	printf("\nThe area of circle is: %f",area);
+//	return 0;
+//}
+
+//Q5. Circumference of circle.
+//#include<stdio.h>
+//#define pie 3.14
+//int main()
+//{
+//	int r;
+//	float cir;
+//	printf("\nEnter radius:");
+//	scanf("%d",&r);
+//	cir=2*pie*r;
+//	printf("\nThe Circumference of circle is: %f",cir);
+//	return 0;
+//}
+
+//Q6. Find the simple interest.
+//#include<stdio.h>
+//int main()
+//{
+//	int p,r,t,si;
+//	printf("\nEnter principal,rate and time:");
+//	scanf("%d %d %d",&p,&r,&t);
+//	si=(p*r*t)/100;
+//	printf("\nThe simple interest is:%d",si);
+//	return 0;
+//}
+
+
+
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int t,h=1189,w=841,i;
+//	for(i=0;i<=8;i++)
+//	{
+//		printf("A%d:%d %d\n",i,h,w);
+//		t=h;
+//		h=w;
+//		w=t/2;
+//	}
+//	return 0;
+//}
+
